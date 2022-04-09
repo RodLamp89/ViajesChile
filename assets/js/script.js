d1fee238e87bf7adbabbb7ffc2c37478a00cc50e
Rodrigo <rodrigo.torresb@mail.udp.cl>
2022-04-09 01:08:52 -0400

    Versión Final

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$("#enviarMensaje").click(function () {
+    alert("El mensaje fue enviado correctamente.")
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
